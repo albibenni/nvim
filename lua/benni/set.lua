bd5636e75e33748cee6ab6d9e8560ef6a384f4ae
Alberto Benatti <albi.benni8@gmail.com>
2024-05-05 15:15:14 +0200

    disable copilot by default

--- lua/benni/set.lua
+++ lua/benni/set.lua
@@ -31,3 +31,8 @@ vim.opt.updatetime = 50
 vim.opt.colorcolumn = "120"
 vim.cmd("highlight ColorColumn ctermbg=235 guibg=#0F0F0F")
 
+vim.api.nvim_create_autocmd("VimEnter", {
+    callback = function()
+        vim.cmd("Copilot disable")
+    end,
+})
