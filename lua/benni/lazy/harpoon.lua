fcb65dba98f3d35a3243f33fb27143988c3fc284
Alberto Benatti <albi.benni8@gmail.com>
2024-07-24 21:20:39 +0200

    fix harpoon and minors

--- lua/benni/lazy/harpoon.lua
+++ lua/benni/lazy/harpoon.lua
@@ -11,14 +11,14 @@ return {
             vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
             vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-            vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
-            vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
-            vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
-            vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
-            vim.keymap.set("n", "<leader><C-h>", function() harpoon:list():replace_at(1) end)
-            vim.keymap.set("n", "<leader><C-t>", function() harpoon:list():replace_at(2) end)
-            vim.keymap.set("n", "<leader><C-n>", function() harpoon:list():replace_at(3) end)
-            vim.keymap.set("n", "<leader><C-s>", function() harpoon:list():replace_at(4) end)
+            vim.keymap.set("n", "<C-u>", function() harpoon:list():select(1) end)
+            vim.keymap.set("n", "<C-i>", function() harpoon:list():select(2) end)
+            vim.keymap.set("n", "<C-o>", function() harpoon:list():select(3) end)
+            vim.keymap.set("n", "<C-p>", function() harpoon:list():select(4) end)
+            vim.keymap.set("n", "<leader><C-u>", function() harpoon:list():replace_at(1) end)
+            vim.keymap.set("n", "<leader><C-i>", function() harpoon:list():replace_at(2) end)
+            vim.keymap.set("n", "<leader><C-o>", function() harpoon:list():replace_at(3) end)
+            vim.keymap.set("n", "<leader><C-p>", function() harpoon:list():replace_at(4) end)
         end
     },
 }
