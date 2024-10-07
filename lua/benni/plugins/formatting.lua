5748818ef880404fb8e78a655dc5cf08922b3ae8
Alberto Benatti <albi.benni8@gmail.com>
2024-10-07 20:32:32 +0200

    some nice stuf

--- lua/benni/plugins/formatting.lua
+++ lua/benni/plugins/formatting.lua
@@ -23,11 +23,12 @@ return {
 				lua = { "stylua" },
 				python = { "isort", "black" },
 			},
-			format_on_save = {
-				lsp_fallback = true,
-				async = false,
-				timeout_ms = 1000,
-			},
+			-- FORMAT ON SAVE
+			-- format_on_save = {
+			-- 	lsp_fallback = true,
+			-- 	async = false,
+			-- 	timeout_ms = 1000,
+			-- },
 		})
 
 		vim.keymap.set({ "n", "v" }, "<leader>f", function()
