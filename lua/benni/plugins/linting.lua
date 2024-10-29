3f567df2b3c83dd5ac4aaf5a2c448e4205622326
Alberto Benatti <albi.benni8@gmail.com>
2024-10-29 09:43:20 +0100

    linting fix

--- lua/benni/plugins/linting.lua
+++ lua/benni/plugins/linting.lua
@@ -5,8 +5,6 @@ return {
 		local lint = require("lint")
 		local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 
-		lint.try_lint(nil, { ignore_errors = true })
-
 		lint.linters_by_ft = {
 			javascript = { "eslint_d", "eslint" },
 			typescript = { "eslint_d", "eslint" },
@@ -34,7 +32,7 @@ return {
 		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
 			group = lint_augroup,
 			callback = function()
-				lint.try_lint()
+				lint.try_lint(nil, { ignore_errors = true })
 			end,
 		})
 
