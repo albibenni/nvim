12664880e9b49e9eda820267953ebec5d3f85d48
Alberto Benatti <albi.benni8@gmail.com>
2024-12-18 14:12:33 +0100

    using oil

--- lua/benni/remap.lua
+++ lua/benni/remap.lua
@@ -1,5 +1,6 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+--vim.keymap.set("n", "<leader>ee", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>ee", ":Oil<CR>")
 
 vim.keymap.set("n", "<leader><CR>", ":luafile %<CR>")
 
