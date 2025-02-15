7e71f634303a61fe877fb8149cff527e7edf61f9
Alberto Benatti <albi.benni8@gmail.com>
2025-02-15 09:53:31 +0100

    remap

--- lua/benni/remap.lua
+++ lua/benni/remap.lua
@@ -48,6 +48,9 @@ vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
+-- Split panes
+vim.keymap.set("n", "<leader>vs", "<cmd>vsplit<CR>")
+
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]]) -- start replacing the word I'm on
 
 vim.keymap.set("n", "<leader>xx", "<cmd>!chmod +x %<CR>", { silent = true }) -- make file executable
