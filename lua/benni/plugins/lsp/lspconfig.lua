8c19e63a402508632bbad0acfcda7b77e467bb2d
Alberto Benatti <albi.benni8@gmail.com>
2025-10-31 10:59:08 +0100

    fix deprecation signs

--- lua/benni/plugins/lsp/lspconfig.lua
+++ lua/benni/plugins/lsp/lspconfig.lua
@@ -8,27 +8,10 @@ return {
 		{ "folke/neodev.nvim", opts = {} },
 		"j-hui/fidget.nvim",
 	},
-	-- opts = {
-	-- 	---@type lspconfig.options
-	-- 	servers = {
-	-- 		eslint = {
-	-- 			settings = {
-	-- 				workingDirectories = { mode = "auto" },
-	-- 				experimental = {
-	-- 					useFlatConfig = true,
-	-- 				},
-	-- 			},
-	-- 		},
-	-- 	},
-	-- },
-	config = function()
-		-- import lspconfig plugin
-		-- local lspconfig = require("lspconfig")
 
-		-- import mason_lspconfig plugin
+	config = function()
 		local mason_lspconfig = require("mason-lspconfig")
 
-		-- import cmp-nvim-lsp plugin
 		local cmp_nvim_lsp = require("cmp_nvim_lsp")
 
 		vim.api.nvim_create_autocmd("LspAttach", {
@@ -85,84 +68,19 @@ return {
 		-- used to enable autocompletion (assign to every lsp server config)
 		local capabilities = cmp_nvim_lsp.default_capabilities()
 
-		-- Change the Diagnostic symbols in the sign column (gutter)
-		-- (not in youtube nvim video)
-		local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
-		for type, icon in pairs(signs) do
-			local hl = "DiagnosticSign" .. type
-			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-		end
+		vim.diagnostic.config({
+			signs = {
+				text = {
+					[vim.diagnostic.severity.ERROR] = " ",
+					[vim.diagnostic.severity.WARN] = " ",
+					[vim.diagnostic.severity.HINT] = "󰠠 ",
+					[vim.diagnostic.severity.INFO] = " ",
+				},
+			},
+		})
 
-		-- mason_lspconfig.setup_handlers({
-		-- 	-- default handler for installed servers
-		-- 	function(server_name)
-		-- 		if server_name == "tsserver" then
-		-- 			server_name = "ts_ls"
-		-- 		end
-		-- 		lspconfig[server_name].setup({
-		-- 			capabilities = capabilities,
-		-- 		})
-		-- 	end,
-		-- 	["svelte"] = function()
-		-- 		-- configure svelte server
-		-- 		lspconfig["svelte"].setup({
-		-- 			capabilities = capabilities,
-		-- 			on_attach = function(client, bufnr)
-		-- 				vim.api.nvim_create_autocmd("BufWritePost", {
-		-- 					pattern = { "*.js", "*.ts" },
-		-- 					callback = function(ctx)
-		-- 						-- Here use ctx.match instead of ctx.file
-		-- 						client.notify("$/onDidChangeTsOrJsFile", { uri = ctx.match })
-		-- 					end,
-		-- 				})
-		-- 			end,
-		-- 		})
-		-- 	end,
-		-- 	["graphql"] = function()
-		-- 		-- configure graphql language server
-		-- 		lspconfig["graphql"].setup({
-		-- 			capabilities = capabilities,
-		-- 			filetypes = { "graphql", "gql", "svelte", "typescriptreact", "javascriptreact" },
-		-- 		})
-		-- 	end,
-		-- 	["emmet_ls"] = function()
-		-- 		-- configure emmet language server
-		-- 		lspconfig["emmet_ls"].setup({
-		-- 			capabilities = capabilities,
-		-- 			filetypes = {
-		-- 				"html",
-		-- 				"typescriptreact",
-		-- 				"javascriptreact",
-		-- 				"css",
-		-- 				"sass",
-		-- 				"scss",
-		-- 				"less",
-		-- 				"svelte",
-		-- 			},
-		-- 		})
-		-- 	end,
-		-- 	["lua_ls"] = function()
-		-- 		-- configure lua server (with special settings)
-		-- 		lspconfig["lua_ls"].setup({
-		-- 			capabilities = capabilities,
-		-- 			settings = {
-		-- 				Lua = {
-		-- 					-- make the language server recognize "vim" global
-		-- 					diagnostics = {
-		-- 						globals = { "vim" },
-		-- 					},
-		-- 					completion = {
-		-- 						callSnippet = "Replace",
-		-- 					},
-		-- 				},
-		-- 			},
-		-- 		})
-		-- 	end,
-		-- })
-
-
-        -- alternative to deprecations
-        -- Configure each server using vim.lsp.config
+		-- alternative to deprecations
+		-- Configure each server using vim.lsp.config
 		local function setup_server(server_name, config)
 			config = config or {}
 			config.capabilities = capabilities
