9aa284634d65ada4420742439196637232709b75
Alberto Benatti <albi.benni8@gmail.com>
2026-04-04 16:44:01 +0200

    remove deprecate jump - new telescope version

--- lua/benni/plugins/telescope.lua
+++ lua/benni/plugins/telescope.lua
@@ -1,6 +1,6 @@
 return {
 	"nvim-telescope/telescope.nvim",
-	branch = "0.1.x",
+	-- branch = "0.1.x",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 		--{ -- If encountering errors, see telescope-fzf-native README for installation instructions
