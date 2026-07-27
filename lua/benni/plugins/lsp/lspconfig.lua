3c86a497895a9221e0c6bace1e90b129d1ae3590
Alberto Benatti <albi.benni8@gmail.com>
2026-07-27 23:19:41 +0200

    fix deprecated

--- lua/benni/plugins/lsp/lspconfig.lua
+++ lua/benni/plugins/lsp/lspconfig.lua
@@ -16,13 +16,7 @@ return {
 		-- Neovim 0.11+ way to style floating windows globally
 		vim.o.winborder = "rounded"
 
-		-- Documentation hover styling
-		vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
-			border = "rounded",
-		})
-		vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
-			border = "rounded",
-		})
+
 
 		vim.api.nvim_create_autocmd("LspAttach", {
 			group = vim.api.nvim_create_augroup("UserLspConfig", {}),
