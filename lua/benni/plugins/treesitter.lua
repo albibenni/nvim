d774682d19b0b75bb4cc26db3f753d3408abedd4
Alberto Benatti <albi.benni8@gmail.com>
2026-07-27 10:26:36 +0200

    align treesitter new

--- lua/benni/plugins/treesitter.lua
+++ lua/benni/plugins/treesitter.lua
@@ -1,25 +1,21 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
-	branch = "main", -- REQUIRED for Neovim 0.12
+	main = "nvim-treesitter.configs",
+	branch = "main",
 	event = { "BufReadPre", "BufNewFile" },
 	build = ":TSUpdate",
 	dependencies = {
 		"windwp/nvim-ts-autotag",
 	},
 	config = function()
-		local treesitter = require("nvim-treesitter")
+		-- Prefer git instead of tarball downloads to avoid 404s (e.g. for jsonc)
+		require("nvim-treesitter.install").prefer_git = true
 
-		-- Register custom templ parser
-		require("nvim-treesitter.parsers").templ = {
-			install_info = {
-				url = "https://github.com/vrischmann/tree-sitter-templ.git",
-				files = { "src/parser.c", "src/scanner.c" },
-				branch = "master",
-			},
-		}
+		-- Autotag must now be setup independently
+		require("nvim-ts-autotag").setup()
 
-		-- List of parsers to ensure are installed
-		local parsers = {
+		-- List of parsers to ensure installed
+		local ensure_installed = {
 			"vimdoc",
 			"javascript",
 			"typescript",
@@ -47,56 +43,19 @@ return {
 			"templ",
 		}
 
-		-- Setup treesitter modules
-		treesitter.setup({
-			ensure_installed = parsers,
-			sync_install = false,
-			auto_install = true,
-			highlight = {
-				enable = true, -- Enable this even in 0.12 for some plugins/modules
-				additional_vim_regex_highlighting = { "markdown" },
-			},
-			indent = {
-				enable = true,
-			},
-			incremental_selection = {
-				enable = true,
-				keymaps = {
-					init_selection = "<C-space>",
-					node_incremental = "<C-space>",
-					scope_incremental = false,
-					node_decremental = "<bs>",
-				},
-			},
-		})
-
-		-- Setup autotag (new way)
-		require("nvim-ts-autotag").setup()
-
-		-- Native Neovim 0.12 Treesitter highlighting
-		vim.api.nvim_create_autocmd("FileType", {
-			callback = function(args)
-				local lang = vim.treesitter.language.get_lang(vim.bo[args.buf].filetype) or vim.bo[args.buf].filetype
-				if lang and pcall(vim.treesitter.language.inspect, lang) then
-					vim.treesitter.start(args.buf, lang)
+		-- Auto-install missing parsers
+		vim.defer_fn(function()
+			local ts = require("nvim-treesitter")
+			local installed = ts.get_installed()
+			local missing = {}
+			for _, p in ipairs(ensure_installed) do
+				if not vim.list_contains(installed, p) then
+					table.insert(missing, p)
 				end
-			end,
-		})
-
-		-- Enable Treesitter highlighting in markdown code blocks (crucial for docs)
-		vim.g.markdown_fenced_languages = {
-			"ts=typescript",
-			"js=javascript",
-			"python",
-			"lua",
-			"rust",
-			"go",
-			"c",
-			"cpp",
-		}
-
-		-- Ensure associations for documentation and help
-		vim.treesitter.language.add("markdown", { filetype = "lazyhelp" })
-		vim.treesitter.language.add("vimdoc", { filetype = "help" })
+			end
+			if #missing > 0 then
+				ts.install(missing)
+			end
+		end, 100)
 	end,
 }
